c5cf1d7a6bf278f0d13e6799e5c2eecc6f357c69
loopByte <milea.vasile959@gmail.com>
2014-02-10 21:58:19 +0200

    Class for admin functionality
    With this class admins can login for the moment

---
+++ classes/admin.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @author MacTavish
+ * @copyright 2014
+ */
+
+    class Admin{
+        public $id;
+        public $name;
+        public $logged;
+        
+        public function __construct(){
+            if(isset($_COOKIE['admin'])){
+                $this->id       = $_COOKIE['id'];
+                $this->name     = $_COOKIE['name'];
+                $this->logged   = true;
+            }
+        }
+        
+        public static function login(){
+            if(isset($_POST['login'])){
+                global $mysqli;
+                
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                $password = md5($password);
+            
+                $query = "SELECT id, full_name, access FROM users WHERE username='".$username."' AND password='".$password."'";
+                $result = $mysqli->query($query);
+                $data = $result->fetch_object();
+                
+                if($data->access == 3){
+                    setcookie('id', $data->id, 0);
+                    setcookie('admin', 1, 0);
+                    setcookie('name', $data->full_name, 0);
+                }else{
+                    exit("You are not an admin");
+                }
+            }
+        }
+    }
+    
+    $admin = new Admin();
+
+?>
