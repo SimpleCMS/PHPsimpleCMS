a64c06754c581419a24b0d63d1d6d57293faf285
loopByte <milea.vasile959@gmail.com>
2014-02-14 00:43:53 +0200

    Those file were removed

--- admin/js/admin.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.admin-nav').mouseenter(function(){
-        $(this).css('background-color', '#5959A9');
-    });
-    $('.admin-nav').mouseleave(function(){
-        $(this).css('background-color', '#00007A');
-    });
-});
