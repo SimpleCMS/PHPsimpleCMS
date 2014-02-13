26eecb4135c32fe8333ae5e7f7e9ef0dddac0e86
loopByte <milea.vasile959@gmail.com>
2014-02-14 00:42:14 +0200

    Script for admin template

---
+++ admin/templates/main/js/admin.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.admin-nav').mouseenter(function(){
+        $(this).css('background-color', '#5959A9');
+    });
+    $('.admin-nav').mouseleave(function(){
+        $(this).css('background-color', '#00007A');
+    });
+});
