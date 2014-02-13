d6281e24e5ed22c431bc5cf5558ba9809c4832e7
loopByte <milea.vasile959@gmail.com>
2014-02-13 20:18:02 +0200

    Template class system
    This class is used to render templates

---
+++ classes/template.php
@@ -0,0 +1,29 @@
+<?php
+    
+    class Template{
+        
+        private $vars = array();
+        
+        public function assign($key, $value){
+            $this->vars[$key] = $value;
+        }
+        
+        public function render($template_name){
+            $path = 'templates/' . $template_name . '/index.php';
+            if(file_exists($path)){
+                $contents = file_get_contents($path);
+                foreach($this->vars as $key => $value){
+                    $contents = preg_replace('/\{' . $key . '\}/', $value, $contents);
+                }
+                $contents = preg_replace('/\<\!\-\- if (.*) \-\-\>/', '<?php if ($1) : ?>', $contents);
+                $contents = preg_replace('/\<\!\-\- else \-\-\>/', '<?php else : ?>', $contents);
+                $contents = preg_replace('/\<\!\-\- endif \-\-\>/', '<?php endif; ?>', $contents);
+                eval(' ?>' . $contents . '<?php ');
+            }else{
+                exit('<h1>Template error</h1>');
+            }
+        }
+        
+    }
+
+?>
