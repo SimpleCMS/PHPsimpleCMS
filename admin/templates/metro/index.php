c8b390c5ae162104b7ecc82da595133c082bd0b2
loopByte <milea.vasile959@gmail.com>
2014-02-14 14:03:11 +0200

    Minor changes to metro template

--- admin/templates/metro/index.php
+++ admin/templates/metro/index.php
@@ -11,26 +11,69 @@
     <head>
         <title><?php Site::title(); ?> - Admin</title>
         <link rel="stylesheet" type="text/css" href="templates/metro/css/main.css" />
-        <link rel="stylesheet" type="text/css" href="templates/metro/css/admin.css" />
-        <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-        <script type="text/javascript" src="templates/metro/js/script.js"></script>
-        <script type="text/javascript" src="templates/metro/js/admin.js"></script>
+        <link rel="stylesheet" type="text/css" href="templates/metro/css/metro-bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="templates/metro/css/metro-bootstrap-responsive.min.css" />
+        <script src="templates/metro/js/metro-ui/metro.min.js"></script>
+        <script src="templates/metro/js/jquery/jquery-1.10.2.js"></script>
+        <script src="templates/metro/js/jquery/jquery.ui.widget.js"></script>
     </head>
-<body>
-    <header class="header">
-        <div>
-            <h1 class="title"><?php Site::title(); ?></h1>
-            <h2 class="description"><em>Admin</em></h2>
-        </div>
-    </header>
-    <div class="page">
-		<div class="menu-container">
-			<h2>Content</h2>
-			<nav class="admin-nav">
-				<a href="#">
-					<h2>Add new article</h2>
-				</a>
-			</nav>
+<body class="metro">
+	<div class="grid fluid">
+		<div class="row">
+			<div class="span12">
+				<h1 class="header">Admin control panel</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="span12">
+				<!-- Content tiles -->
+				<div class="tile-group two fg-black">
+					<div class="tile-group-title">Content</div>
+					<a href="index.php?page=article&action=new" class="tile bg-teal">
+						<div class="tile-status">
+							<span class="name">Add new article</span>
+						</div>
+					</a>
+					<a href="index.php?page=article&action=manage" class="tile bg-teal"> 
+						<div class="tile-status">
+							<span class="name">Article manager</span>
+						</div>
+					</a>
+					<a href="index.php?page=article&action=category" class="tile double bg-teal">
+						<div class="tile-status">
+							<span class="name">Category manager</span>
+						</div>
+					</a>
+				</div>
+				<!-- End content tiles -->
+				
+				<!-- Users tile -->
+				<div class="tile-group one fg-black">
+					<div class="tile-group-title">Users</div>
+					<a href="index.php?page=user&action=manage" class="tile bg-red">
+						<div class="tile-status">
+							<span class="name">User manager</span>
+						</div>
+					</a>
+				</div>
+				<!-- End users tile -->
+				
+				<!-- Configuration tile -->
+				<div class="tile-group two fg-black">
+					<div class="tile-group-title">Configuration</div>
+					<a href="index.php?page=global_configuration" class="tile bg-orange">
+						<div class="tile-status">
+							<span class="name">Global configuration</span>
+						</div>
+					</a>
+					<a href="index.php?page=template&action=manage" class="tile bg-orange">
+						<div class="tile-status">
+							<span class="name">Template manager</span>
+						</div>
+					</a>
+				</div>
+				<!-- End configuration tile -->
+			</div>
 		</div>
 	</div>
 </body>
