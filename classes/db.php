70705a50736cd8a8aaf81538895e99111138c806
loopByte <milea.vasile959@gmail.com>
2014-02-17 20:43:24 +0000

    Minor changes

--- classes/db.php
+++ classes/db.php
@@ -5,7 +5,7 @@
  * @copyright 2014
  */
 
-	$mysqli = new mysqli("127.0.0.1", "root", "", "cms");
+	$mysqli = new mysqli("127.0.0.1", "simplecms", "simplecms", "cms");
 	
 	if($mysqli->connect_errno){
 		exit($mysqli->connect_error);
